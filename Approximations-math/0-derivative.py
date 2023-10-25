e2d19d39ca1053076ee2852706e39fd15aed7b59
khaled <khaled.gaguiga@holbertonschool.com>
2023-10-25 17:13:44 +0100

    app mat

--- Approximations-math/0-derivative.py
+++ Approximations-math/0-derivative.py
@@ -6,12 +6,12 @@ def identify_best_k(funcs, funcs_prime, xs):
     ks = np.arange(1, 53)  # Numpy array of integers in the range [1, 52]
 
     # Calculate the result
-    results = np.array([
+    results = np.array(
         [
             [abs((funcs[i](xs + (2.0 ** -k)) - funcs[i](xs)) / (2.0 ** -k) - funcs_prime[i](xs)) for k in ks]
             for i in range(len(funcs))
         ]
-    ])
+    )
 
     # Find the index (k) that corresponds to the minimum value for each func and x
     min_indices = np.argmin(results, axis=2) # Adding 1 to make the indices in the range [1, 52]
